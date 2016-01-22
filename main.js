e0255c943ed353c191682c394410f49953739c84
Xavier Garcia Buils <xgbuils@gmail.com>
2016-01-22 10:20:39 +0100

    asd

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function createList (list) {
 }
 
 function createDateElement (dateString) {
-    var date = new Date(dateString)
+    var date = createDateFromString(dateString)
     var hours = date.getHours()
     var dl = document.createElement('dl')
     if (isNan(hours)) {
@@ -17,7 +17,6 @@ function createDateElement (dateString) {
     }
     dl.appendChild(createElement('dt', 'String'))
     dl.appendChild(createElement('dd', dateString))
-    //dl.appendChild(document.createTextNode(' '))
     dl.appendChild(createElement('dt', 'Date'))
     dl.appendChild(createElement('dd', date))
     dl.appendChild(createElement('dt', 'Hours'))
@@ -35,6 +34,15 @@ function createElement (tag, text) {
 function isNan (num) {
     return num !== num
 }
+
+var createDateFromString = function (dateString) {
+    var returnDate = new Date(dateString);
+    if (!(returnDate instanceof Date) || isNaN(returnDate.valueOf())) {
+        var pattern = /([0-9-]+) ([0-9:]+) (\+[0-9][0-9])([0-9][0-9])/i; //2014-06-18 17:35:28 +0200
+        returnDate = new Date(dateString.replace(pattern, '$1T$2$3:$4'));//2014-06-18T17:35:28+02:00
+    }
+    return returnDate;
+};
 
 window.onload = function () {
     document.body.appendChild(createList([
